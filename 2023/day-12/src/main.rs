64f73b7273af373c4120ea46eaec4a4001c994c2
Sanjay Ginde <sanjayginde@gmail.com>
2023-12-12 14:41:08 -0500

    More efficient combination finder, but still too slow for part 2

--- 2023/day-12/src/main.rs
+++ 2023/day-12/src/main.rs
@@ -8,21 +8,31 @@ fn parse(lines: Vec<String>) -> Vec<Row> {
     lines.into_iter().map(Row::from).collect()
 }
 
-fn solve(rows: Vec<Row>) -> usize {
+fn solve_part1(rows: Vec<Row>) -> usize {
     rows.into_iter()
         .fold(0, |acc, r| acc + r.calc_arrangements())
+}
+
+fn solve_part2(rows: Vec<Row>) -> usize {
+    let rows: Vec<Row> = rows
+        .into_iter()
+        .map(|row| row.convert_to_part2_row())
+        .collect();
+
+    solve_part1(rows)
 }
 
 fn main() {
     let rows = parse(read_lines("input.txt"));
 
-    println!("Solution for part 1 is {}", solve(rows));
+    println!("Solution for part 1 is {}", solve_part1(rows.clone()));
+    println!("Solution for part 2 is {}", solve_part2(rows));
 }
 
 #[cfg(test)]
 mod test {
 
-    use super::{parse, solve};
+    use super::{parse, solve_part1, solve_part2};
 
     const EXAMPLE: [&str; 6] = [
         "???.### 1,1,3",
@@ -34,10 +44,17 @@ mod test {
     ];
 
     #[test]
-    fn solve_example() {
+    fn solve_example_part1() {
         let rows = parse(EXAMPLE.map(String::from).to_vec());
 
-        assert_eq!(solve(rows), 21);
+        assert_eq!(solve_part1(rows), 21);
+    }
+
+    #[test]
+    fn solve_example_part2() {
+        let rows = parse(EXAMPLE.map(String::from).to_vec());
+
+        assert_eq!(solve_part2(rows), 525152);
     }
 }
 
--- 2023/day-12/src/springs.rs
+++ 2023/day-12/src/springs.rs
@@ -3,57 +3,23 @@ use regex::Regex;
 #[derive(Debug, Clone)]
 pub struct Row {
     springs: String,
+    damaged_groupings: Vec<usize>,
     arrangements_pattern: Regex,
 }
 
-fn generate_combinations(springs: String) -> Vec<String> {
-    match springs.split_once("?") {
-        None => vec![springs],
-        Some((lhs, rhs)) => {
-            let mut result: Vec<String> = Vec::new();
-            for combo in generate_combinations(format!("#{rhs}")).into_iter() {
-                result.push(format!("{lhs}{combo}"));
-            }
-
-            for combo in generate_combinations(format!(".{rhs}")).into_iter() {
-                result.push(format!("{lhs}{combo}"));
-            }
-
-            result
-        }
-    }
-}
-
 impl Row {
-    pub fn calc_arrangements(&self) -> usize {
-        let combinations = generate_combinations(self.springs.clone());
-        combinations.into_iter().fold(0, |acc, combo| {
-            match self.arrangements_pattern.is_match(combo.as_str()) {
-                true => acc + 1,
-                false => acc,
-            }
-        })
-    }
-}
-
-impl From<String> for Row {
-    fn from(value: String) -> Self {
-        let splits: Vec<_> = value.split_whitespace().collect();
-
-        let springs = splits[0].to_string();
-        let groupings: Vec<&str> = splits[1].split(",").collect();
-
-        let last_index = groupings.len() - 1;
-        let mut regex = "^\\.*".to_string();
-        for (i, grouping) in groupings.iter().enumerate() {
-            regex.push_str(format!("#{{{grouping}}}").as_str());
+    pub fn new(springs: String, damaged_groupings: Vec<usize>) -> Self {
+        let last_index = damaged_groupings.len() - 1;
+        let mut regex = "^[^#]*".to_string();
+        for (i, grouping) in damaged_groupings.iter().enumerate() {
+            regex.push_str(format!("[^.]{{{grouping}}}").as_str());
 
             if i != last_index {
-                regex.push_str("\\.+");
+                regex.push_str("[^#]+");
             }
         }
 
-        regex.push_str("\\.*$");
+        regex.push_str("[^#]*$");
 
         // println!("regex: {regex}");
 
@@ -64,8 +30,97 @@ impl From<String> for Row {
 
         Row {
             springs,
+            damaged_groupings,
             arrangements_pattern,
         }
+    }
+
+    pub fn springs(&self) -> &String {
+        &self.springs
+    }
+
+    pub fn damaged_groupings(&self) -> &Vec<usize> {
+        &self.damaged_groupings
+    }
+
+    pub fn calc_arrangements(&self) -> usize {
+        let combinations = self.generate_combinations(String::new(), self.springs.clone());
+
+        // println!("# combos: {}", combinations.len());
+        // println!("combos: {:?}", combinations);
+        combinations.len()
+
+        // combinations.into_iter().fold(0, |acc, combo| {
+        //     match self.arrangements_pattern.is_match(combo.as_str()) {
+        //         true => acc + 1,
+        //         false => acc,
+        //     }
+        // })
+    }
+
+    fn generate_combinations(&self, lhs: String, rhs: String) -> Vec<String> {
+        if !self
+            .arrangements_pattern
+            .is_match(format!("{lhs}{rhs}").as_str())
+        {
+            // println!("nomatch: {lhs}{rhs}");
+            return vec![];
+        }
+
+        match rhs.split_once("?") {
+            None => vec![format!("{lhs}{rhs}")],
+            Some((l, r)) => {
+                let mut result: Vec<String> = Vec::new();
+                for combo in self
+                    .generate_combinations(format!("{lhs}{l}"), format!("#{r}"))
+                    .into_iter()
+                {
+                    result.push(format!("{lhs}{l}{combo}"));
+                }
+
+                for combo in self
+                    .generate_combinations(format!("{lhs}{l}"), format!(".{r}"))
+                    .into_iter()
+                {
+                    result.push(format!("{lhs}{l}{combo}"));
+                }
+
+                result
+            }
+        }
+    }
+
+    pub fn convert_to_part2_row(&self) -> Row {
+        let mut springs = String::with_capacity(self.springs().len() * 5);
+        let mut damaged_groupings: Vec<usize> =
+            Vec::with_capacity(self.damaged_groupings().len() * 5);
+        for _ in (0..4).into_iter() {
+            springs.push_str(self.springs().as_str());
+            springs.push('?');
+
+            damaged_groupings.append(&mut self.damaged_groupings().clone())
+        }
+
+        springs.push_str(self.springs().as_str());
+        damaged_groupings.append(&mut self.damaged_groupings().clone());
+
+        Row::new(springs, damaged_groupings)
+    }
+}
+
+impl From<String> for Row {
+    fn from(value: String) -> Self {
+        let splits: Vec<_> = value.split_whitespace().collect();
+
+        let springs = splits[0].to_string();
+        let groupings: Vec<&str> = splits[1].split(",").collect();
+
+        let damaged_groupings: Vec<usize> = groupings
+            .iter()
+            .map(|grouping| grouping.parse().unwrap())
+            .collect();
+
+        Row::new(springs, damaged_groupings)
     }
 }
 
@@ -87,10 +142,11 @@ mod test {
 
         let row = Row::from(line);
 
-        assert_eq!(row.springs, "???.###");
+        assert_eq!(row.springs(), "???.###");
+        assert_eq!(row.damaged_groupings(), &vec![1, 1, 3]);
         assert_eq!(
             row.arrangements_pattern.as_str(),
-            r"^\.*#{1}\.+#{1}\.+#{3}\.*$"
+            r"^[^#]*[^.]{1}[^#]+[^.]{1}[^#]+[^.]{3}[^#]*$"
         );
     }
 
@@ -103,6 +159,31 @@ mod test {
     #[case("?###???????? 3,2,1", 10)]
     fn calc_arrangements(#[case] row: &str, #[case] expected: usize) {
         let row = Row::from(row);
+
+        assert_eq!(row.calc_arrangements(), expected);
+    }
+
+    #[test]
+    fn convert_to_part2_row() {
+        let line = "???.### 1,1,3".to_string();
+        let row = Row::from(line).convert_to_part2_row();
+
+        assert_eq!(row.springs(), "???.###????.###????.###????.###????.###");
+        assert_eq!(
+            row.damaged_groupings(),
+            &vec![1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3]
+        );
+    }
+
+    #[rstest]
+    #[case("???.### 1,1,3", 1)]
+    #[case(".??..??...?##. 1,1,3", 16384)]
+    #[case("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+    #[case("????.#...#... 4,1,1", 16)]
+    #[case("????.######..#####. 1,6,5", 2500)]
+    #[case("?###???????? 3,2,1", 506250)]
+    fn calc_arrangements_part2(#[case] row: &str, #[case] expected: usize) {
+        let row = Row::from(row).convert_to_part2_row();
 
         assert_eq!(row.calc_arrangements(), expected);
     }
